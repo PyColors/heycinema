35280e579795045dc0fe4b5edf55cd67a3433992
PyColors <patweb44@gmail.com>
2019-09-15 21:25:18 +0100

    Setting up node server

---
+++ server/util/logger.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+
+const chalk = require('chalk');
+const ip = require('ip');
+
+const divider = chalk.gray('\n-----------------------------------');
+
+// Logger middleware, can customize to make messages more personal
+const logger = {
+  // Called whenever there's an error on the server to print
+  error: (err) => {
+    console.error(chalk.red(err));
+  },
+
+  // Called when express.js app starts on given port w/o errors
+  appStarted: (port, host) => {
+    console.log(`Server started ! ${chalk.green('✓')}`);
+
+    console.log(`
+${chalk.bold('Access URLs:')}${divider}
+Localhost: ${chalk.magenta(`http://${host}:${port}`)}
+      LAN: ${chalk.magenta(`http://${ip.address()}:${port}`)}${divider}
+${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}
+    `);
+  }
+};
+
+module.exports = logger;
