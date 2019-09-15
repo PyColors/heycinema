578b84db074fe4c0714b097120307e0a1eb1959d
PyColors <patweb44@gmail.com>
2019-09-15 21:11:52 +0100

    Ass entry file for the application

---
+++ src/app.js
@@ -0,0 +1,54 @@
+/**
+ * This is the entry file for the application
+ */
+
+// Needed for redux-saga es6 generator support
+import '@babel/polyfill';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import history from 'utils/history';
+import 'sanitize.css/sanitize.css';
+
+// Import root app
+import App from '../src/package/templates/App';
+
+// Load the favicon
+/* eslint-disable import/no-webpack-loader-syntax */
+import '!file-loader?name=[name].[ext]!./images/favicon.ico';
+/* eslint-enable import/no-webpack-loader-syntax */
+
+// Import CSS
+import './styles/index.scss';
+
+import configureStore from './configureStore';
+
+// Create redux store with history
+const initialState = {};
+const store = configureStore(initialState, history);
+const MOUNT_NODE = document.getElementById('root');
+
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>,
+    MOUNT_NODE
+  );
+};
+
+if (module.hot) {
+  // Hot reloadable React components and translation json files
+  // modules.hot.accept does not accept dynamic dependencies,
+  // have to be constants at compile-time
+  module.hot.accept(['../src/package/templates/App'], () => {
+    ReactDOM.unmountComponentAtNode(MOUNT_NODE);
+    render();
+  });
+}
+
+render();
