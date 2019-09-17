ea007a822cf7b685020fa219051d21dc3aa72971
PyColors <patweb44@gmail.com>
2019-09-17 19:20:13 +0100

    Rename actions

--- app/package/pages/HomePage/constants.js
+++ app/package/pages/HomePage/constants.js
@@ -1,1 +1,1 @@
-export const CHANGE_TITLE = 'boilerplate/Home/CHANGE_TITLE';
+export const CHANGE_TITLE = 'heycinema/Home/CHANGE_TITLE';
--- app/package/templates/App/constants.js
+++ app/package/templates/App/constants.js
@@ -1,4 +1,4 @@
-export const LOAD_MOVIES = 'boilerplate/App/LOAD_MOVIES';
-export const LOAD_MOVIES_SUCCESS = 'boilerplate/App/LOAD_MOVIES_SUCCESS';
-export const LOAD_MOVIES_ERROR = 'boilerplate/App/LOAD_MOVIES_ERROR';
+export const LOAD_MOVIES = 'heycinema/App/LOAD_MOVIES';
+export const LOAD_MOVIES_SUCCESS = 'heycinema/App/LOAD_MOVIES_SUCCESS';
+export const LOAD_MOVIES_ERROR = 'heycinema/App/LOAD_MOVIES_ERROR';
 export const DEFAULT_LOCALE = 'en';
