f0bd9989cce7c99220b9f5d28e8bb88dbd7bf146
Py Colors <patweb44@gmail.com>
2024-06-12 14:37:38 +0200

    feat: update not found page

--- app/package/pages/NotFoundPage/NotFoundPage.js
+++ app/package/pages/NotFoundPage/NotFoundPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
   return (
     <article className="not-found">
       <h1>Page not found.</h1>
+      <p>Sorry, the page you were trying to view does not exist.</p>
+      <Link to="/">Return to Home Page</Link>
     </article>
   );
 }
