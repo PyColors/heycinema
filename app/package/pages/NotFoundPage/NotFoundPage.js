dce0cdc6290ef4b18643a5cc94c5542bd000558a
Py Colors <patweb44@gmail.com>
2024-06-14 11:52:36 +0200

    Update NotFound page as a test

--- app/package/pages/NotFoundPage/NotFoundPage.js
+++ app/package/pages/NotFoundPage/NotFoundPage.js
@@ -1,11 +1,22 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {NavLink } from 'react-router-dom';
 
-export default function NotFound() {
+/**
+ * Functional component for rendering a "Page not found" message with a link to return to the Home Page.
+ */
+const NotFound = () => {
   return (
+    <main className="not-found">
       <h1>Page not found.</h1>
       <p>Sorry, the page you were trying to view does not exist.</p>
-      Return to Home Page
-    </article>
+      <NavLink aria-label="Return to Home Page" to="/">
+        Return to Home Page
+      </NavLink>
+    </main>
   );
 }
+
+NotFound.propTypes = {
+  // Define prop types here
+};
